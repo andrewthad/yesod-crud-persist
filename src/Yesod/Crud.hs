0066fc5c29810359d13f2528526b9b970932450c
Andrew <andrew.thaddeus@gmail.com>
2015-08-16 14:37:51 -0400

    Generalized everything

--- src/Yesod/Crud.hs
+++ src/Yesod/Crud.hs
@@ -4,65 +4,19 @@ import Prelude
 import Control.Applicative
 import Data.Maybe
 
+import Lens.Micro.TH
+
 import Yesod.Core
 import Database.Persist(Key)
 import Network.Wai (pathInfo, requestMethod)
 import qualified Data.List as List
+import Yesod.Persist
+import Database.Persist.Sql
+import Data.Foldable (for_)
 
 import Yesod.Crud.Internal
 
-data Crud master a = Crud
-  { _chAdd    :: HandlerT (Crud master a) (HandlerT master IO) Html
-  , _chIndex  :: HandlerT (Crud master a) (HandlerT master IO) Html
-  , _chEdit   :: Key a -> HandlerT (Crud master a) (HandlerT master IO) Html
-  , _chDelete :: Key a -> HandlerT (Crud master a) (HandlerT master IO) Html
-  }
-
--- Dispatch for the crud subsite
-instance (Eq (Key a), PathPiece (Key a)) => YesodSubDispatch (Crud master a) (HandlerT master IO) where
-  yesodSubDispatch env req = h
-    where 
-    h = let parsed = parseRoute (pathInfo req, []) 
-            helper a = subHelper (fmap toTypedContent a) env parsed req
-        in case parsed of
-          Just (EditR theId)   -> onlyAllow ["GET","POST"] $ helper $ 
-                                  getYesod >>= (\s -> _chEdit s theId)
-          Just (DeleteR theId) -> onlyAllow ["GET","POST"] $ helper $ 
-                                  getYesod >>= (\s -> _chDelete s theId)
-          Just AddR            -> onlyAllow ["GET","POST"] $ helper $
-                                  getYesod >>= _chAdd
-          Just IndexR          -> onlyAllow ["GET"] $ helper $
-                                  getYesod >>= _chIndex
-          Nothing              -> notFoundApp
-    onlyAllow reqTypes waiApp = if isJust (List.find (== requestMethod req) reqTypes) then waiApp else notFoundApp
-    notFoundApp = subHelper (fmap toTypedContent notFoundUnit) env Nothing req
-    notFoundUnit = fmap (\() -> ()) notFound
-
-instance (PathPiece (Key a), Eq (Key a)) => RenderRoute (Crud master a) where
-  data Route (Crud master a)
-    = EditR (Key a)
-    | DeleteR (Key a)
-    | IndexR
-    | AddR
-  renderRoute r = noParams $ case r of
-    EditR theId   -> ["edit", toPathPiece theId]
-    DeleteR theId -> ["delete", toPathPiece theId]
-    IndexR        -> ["index"]
-    AddR          -> ["add"]
-    where noParams xs = (xs,[])
-
-instance (Eq (Key a), PathPiece (Key a)) => ParseRoute (Crud master a) where
-  parseRoute (_, (_:_)) = Nothing
-  parseRoute (xs, []) = Nothing
-    <|> (runSM xs $ pure EditR <* consumeMatchingText "edit" <*> consumeKey)
-    <|> (runSM xs $ pure DeleteR <* consumeMatchingText "delete" <*> consumeKey)
-    <|> (runSM xs $ pure IndexR <* consumeMatchingText "index")
-    <|> (runSM xs $ pure AddR <* consumeMatchingText "add")
-
-
-deriving instance Eq (Key a) => Eq (Route (Crud master a))
-deriving instance Show (Key a) => Show (Route (Crud master a))
-deriving instance Read (Key a) => Read (Route (Crud master a))
+type Crud master a = ChildCrud master () a
 
 -- In ChildCrud, c is the child type, and p is the type of the identifier
 -- for its parent.
@@ -72,8 +26,7 @@ data ChildCrud master p c = ChildCrud
   , _ccEdit   :: Key c -> HandlerT (ChildCrud master p c) (HandlerT master IO) Html
   , _ccDelete :: Key c -> HandlerT (ChildCrud master p c) (HandlerT master IO) Html
   }
-
-type HierarchyCrud master a = ChildCrud master (Maybe (Key a)) a
+makeLenses ''ChildCrud
 
 -- Dispatch for the child crud subsite
 instance (Eq (Key c), PathPiece (Key c), Eq p, PathPiece p) => YesodSubDispatch (ChildCrud master p c) (HandlerT master IO) where
@@ -120,5 +73,66 @@ deriving instance (Eq (Key c), Eq p) => Eq (Route (ChildCrud master p c))
 deriving instance (Show (Key c), Show p) => Show (Route (ChildCrud master p c))
 deriving instance (Read (Key c), Read p) => Read (Route (ChildCrud master p c))
 
+type HierarchyCrud master a = ChildCrud master (Maybe (Key a)) a
+
+class (NodeTable c ~ a, ClosureTable a ~ c) => ClosureTablePair a c where
+  type NodeTable c
+  type ClosureTable a
+  closureAncestorCol :: EntityField c (Key a)
+  closureDescendantCol :: EntityField c (Key a)
+  closureDepthCol :: EntityField c Int
+  closureAncestor :: c -> Key a
+  closureDescendant :: c -> Key a
+  closureDepth :: c -> Int
+  closureCreate :: Key a -> Key a -> Int -> c
+
+type PersistCrudEntity master a =
+  ( PathPiece (Key a) 
+  , Yesod master
+  , YesodPersist master
+  , PersistEntity a
+  , PersistQuery (YesodPersistBackend master)
+  , PersistEntityBackend a ~ YesodPersistBackend master
+  )
+
+type SqlClosure a c = 
+  ( ClosureTablePair a c
+  , PersistEntityBackend a ~ SqlBackend
+  , PersistEntityBackend (ClosureTable a) ~ SqlBackend
+  , PersistEntity a
+  , PersistEntity c
+  , PersistField (Key a)
+  )
+
+closureDepthColAs :: forall a c. ClosureTablePair a c 
+  => Key a -> EntityField c Int
+closureDepthColAs _ = (closureDepthCol :: EntityField c Int)
+
+closureGetImmidiateChildren :: (MonadIO m, SqlClosure a c) 
+   => Key a -> SqlPersistT m [Entity a]
+closureGetImmidiateChildren theId = do
+  cs <- selectList [closureAncestorCol ==. theId, closureDepthCol ==. 1] []
+  selectList [persistIdField <-. map (closureDescendant . entityVal) cs] [Asc persistIdField]
+
+closureGetParentId :: (MonadIO m, SqlClosure a c) 
+   => Key a -> SqlPersistT m (Maybe (Key a))
+closureGetParentId theId = do
+  cs <- selectList [closureDescendantCol ==. theId, closureDepthCol ==. 1] []
+  return $ fmap (closureAncestor . entityVal) $ listToMaybe cs
+
+closureGetParentIdProxied :: (MonadIO m, SqlClosure a c) 
+   => p c -> Key a -> SqlPersistT m (Maybe (Key a))
+closureGetParentIdProxied _ = closureGetParentId
+
+closureInsert :: forall m a c. (MonadIO m, SqlClosure a c) 
+  => Maybe (Key a) -> a -> SqlPersistT m (Key a)
+closureInsert mparent a = do
+  childId <- insert a
+  _ <- insert $ closureCreate childId childId 0 
+  for_ mparent $ \parentId -> do
+    cs <- selectList [closureDescendantCol ==. parentId] []
+    insertMany_ $ map (\(Entity _ c) -> 
+      closureCreate (closureAncestor c) childId (closureDepth c + 1)) cs 
+  return childId
 
 
--- src/Yesod/Crud/Simple.hs
+++ src/Yesod/Crud/Simple.hs
@@ -2,40 +2,77 @@ module Yesod.Crud.Simple where
 
 import Prelude
 import Data.Monoid
-import Control.Lens.TH
-import Control.Lens hiding (index)
+import Lens.Micro
+import Lens.Micro.TH
 
 import Yesod.Core
 import Yesod.Form
 import Yesod.Persist
+import Database.Persist.Sql
 import Data.Text (Text)
+import Control.Monad
+import Data.Proxy
 
 import Yesod.Crud
 
-data SimpleCrud master a = SimpleCrud
-  { _scAdd        :: WidgetT master IO () -> HandlerT (Crud master a) (HandlerT master IO) Html
-  , _scIndex      :: HandlerT (Crud master a) (HandlerT master IO) Html
-  , _scEdit       :: WidgetT master IO () -> HandlerT (Crud master a) (HandlerT master IO) Html
-  , _scDelete     :: WidgetT master IO () -> HandlerT (Crud master a) (HandlerT master IO) Html
+data SimpleCrud master p c = SimpleCrud
+  { _scAdd        :: WidgetT master IO () -> HandlerT (ChildCrud master p c) (HandlerT master IO) Html
+  , _scIndex      :: p -> HandlerT (ChildCrud master p c) (HandlerT master IO) Html
+  , _scEdit       :: WidgetT master IO () -> HandlerT (ChildCrud master p c) (HandlerT master IO) Html
+  , _scDelete     :: WidgetT master IO () -> HandlerT (ChildCrud master p c) (HandlerT master IO) Html
   , _scDeleteForm :: WidgetT master IO () 
-  , _scForm       :: Maybe a -> Html -> MForm (HandlerT master IO) (FormResult a, WidgetT master IO ())
+  , _scForm       :: Maybe c -> Html -> MForm (HandlerT master IO) (FormResult c, WidgetT master IO ())
   , _scFormWrap   :: Enctype -> Route master -> WidgetT master IO () -> WidgetT master IO ()
+  , _scDeleteDb   :: Key c -> YesodDB master p
+  , _scAddDb      :: p -> c -> YesodDB master (Key c)
+  , _scEditDb     :: Key c -> c -> YesodDB master p
   }
 makeLenses ''SimpleCrud
 
-emptySimpleCrud :: SimpleCrud master a
-emptySimpleCrud = SimpleCrud (const $ return mempty) (return mempty) (const $ return mempty) (const $ return mempty) 
-                  mempty (const $ const $ return (FormMissing,mempty)) (const $ const $ const mempty) 
-
-basicSimpleCrud :: forall master a. 
+emptyParentlessSimpleCrud :: 
      PathPiece (Key a) 
   => Yesod master
   => YesodPersist master
   => PersistEntity a
   => PersistQuery (YesodPersistBackend master)
   => PersistEntityBackend a ~ YesodPersistBackend master
-  => SimpleCrud master a
-basicSimpleCrud = emptySimpleCrud
+  => SimpleCrud master () a
+emptyParentlessSimpleCrud = SimpleCrud 
+  (const $ return mempty)  -- add 
+  (const $ return mempty)          -- index
+  (const $ return mempty)  -- edit
+  (const $ return mempty)  -- delete
+  mempty (const $ const $ return (FormMissing,mempty)) -- delete form
+  (const $ const $ const mempty) -- form wrapper
+  delete -- default deletion, assumes no FK constraints
+  (const insert) -- default DB add
+  replace -- default DB edit
+
+emptyHierarchySimpleCrud :: forall a c master.
+     SqlBackend ~ YesodPersistBackend master
+  => PersistStore (YesodPersistBackend master)
+  => YesodPersist master
+  => SqlClosure a c
+  => SimpleCrud master (Maybe (Key a)) a 
+emptyHierarchySimpleCrud = SimpleCrud
+  (const $ return mempty)  -- add 
+  (const $ return mempty)  -- index
+  (const $ return mempty)  -- edit
+  (const $ return mempty)  -- delete
+  mempty (const $ const $ return (FormMissing,mempty)) -- delete form
+  (const $ const $ const mempty) -- form wrapper
+  del -- deletion
+  closureInsert -- default DB add
+  edit -- default DB edit
+  where 
+  del k = closureGetParentIdProxied (Proxy :: Proxy c) k
+  edit k v = do
+    replace k v
+    closureGetParentIdProxied (Proxy :: Proxy c) k
+
+applyBasicLayoutsAndForms :: PersistCrudEntity master a
+  => SimpleCrud master p a -> SimpleCrud master p a
+applyBasicLayoutsAndForms initial = initial
   & scIndex      .~ index
   & scAdd        .~ lift . defaultLayout
   & scEdit       .~ lift . defaultLayout
@@ -46,15 +83,13 @@ basicSimpleCrud = emptySimpleCrud
           <form action="@{route}" enctype="#{enctype}" method="post">
             ^{inner}
         |]
-        index :: HandlerT (Crud master a) (HandlerT master IO) Html 
-        index = do
+        index p = do
           tp <- getRouteToParent
           as <- lift $ runDB $ selectList [] []
-          let _ = as :: [Entity a]
           lift $ defaultLayout $ [whamlet|$newline never
             <h1>Index
             <p>
-              <a href="@{tp AddR}">Add
+              <a href="@{tp (ChildAddR p)}">Add
             <table>
               <thead>
                 <tr>
@@ -66,19 +101,27 @@ basicSimpleCrud = emptySimpleCrud
                   <tr>
                     <td>#{toPathPiece theId}
                     <td>
-                      <a href="@{tp (EditR theId)}">Edit
+                      <a href="@{tp (ChildEditR theId)}">Edit
                     <td>
-                      <a href="@{tp (DeleteR theId)}">Delete
+                      <a href="@{tp (ChildDeleteR theId)}">Delete
           |]
 
-simpleCrudToCrud :: PersistEntityBackend a ~ YesodPersistBackend master
+basicSimpleCrud :: PersistCrudEntity master a => SimpleCrud master () a
+basicSimpleCrud = applyBasicLayoutsAndForms emptyParentlessSimpleCrud
+
+basicHierarchySimpleCrud :: (PersistCrudEntity master a, SqlClosure a c)
+  => SimpleCrud master (Maybe (Key a)) a
+basicHierarchySimpleCrud = applyBasicLayoutsAndForms emptyHierarchySimpleCrud
+
+simpleCrudToCrud :: 
+     PersistEntityBackend a ~ YesodPersistBackend master
   => PersistEntity a
   => PersistStore (YesodPersistBackend master)
   => YesodPersist master
   => RenderMessage master FormMessage
-  => SimpleCrud master a -> Crud master a
-simpleCrudToCrud (SimpleCrud add index edit del delForm form wrap) = 
-  Crud addH indexH editH delH
+  => SimpleCrud master p a -> ChildCrud master p a
+simpleCrudToCrud (SimpleCrud add index edit del delForm form wrap delDb addDb editDb) = 
+  ChildCrud addH indexH editH delH
   where 
   indexH = index
   delH theId = do
@@ -87,22 +130,22 @@ simpleCrudToCrud (SimpleCrud add index edit del delForm form wrap) =
       res <- runInputPostResult $ ireq textField "fake"
       case res of
         FormSuccess _ -> do
-          runDB $ delete theId
+          p <- runDB $ delDb theId
           setMessageI ("You have deleted the resource." :: Text)
-          redirect (tp IndexR)
+          redirect (tp $ ChildIndexR p)
         _ -> return ()
-    del (wrap UrlEncoded (tp $ DeleteR theId) ([whamlet|<input type="hidden" value="a" name="fake">|] <> delForm))
-  addH = do 
+    del (wrap UrlEncoded (tp $ ChildDeleteR theId) ([whamlet|<input type="hidden" value="a" name="fake">|] <> delForm))
+  addH p = do 
     tp <- getRouteToParent
     (enctype,w) <- lift $ do
       ((res,w),enctype) <- runFormPost (form Nothing)
       case res of
         FormSuccess a -> do
-          runDB $ insert_ a 
+          void $ runDB $ addDb p a 
           setMessageI ("You have created a new resource." :: Text)
-          redirect (tp IndexR)
+          redirect (tp $ ChildIndexR p)
         _ -> return (enctype,w)
-    add (wrap enctype (tp AddR) w)
+    add (wrap enctype (tp $ ChildAddR p) w)
   editH theId = do
     tp <- getRouteToParent
     (enctype,w) <- lift $ do
@@ -110,9 +153,9 @@ simpleCrudToCrud (SimpleCrud add index edit del delForm form wrap) =
       ((res,w),enctype) <- runFormPost (form $ Just old)
       case res of
         FormSuccess new -> do
-          runDB $ replace theId new
+          p <- runDB $ editDb theId new
           setMessageI ("You have updated the resource." :: Text)
-          redirect (tp IndexR)
+          redirect (tp $ ChildIndexR p)
         _ -> return (enctype,w)
-    edit (wrap enctype (tp $ EditR theId) w)
+    edit (wrap enctype (tp $ ChildEditR theId) w)
 
